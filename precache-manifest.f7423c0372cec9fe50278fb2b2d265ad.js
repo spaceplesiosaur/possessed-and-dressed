541bb660dcc2760fcc97367a794aea6658743feb
Allie McCarthy <mccarthy.allie@gmail.com>
2020-01-31 12:43:02 -0700

    Updates

---
+++ precache-manifest.f7423c0372cec9fe50278fb2b2d265ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b779bee1753ace58de1e1f2df09a5f3e",
+    "url": "/possessed-and-dressed/index.html"
+  },
+  {
+    "revision": "7f86d4c9f0455a68e391",
+    "url": "/possessed-and-dressed/static/css/main.13d07546.chunk.css"
+  },
+  {
+    "revision": "c6144b9e3cb3a746ea2b",
+    "url": "/possessed-and-dressed/static/js/2.3056a990.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/possessed-and-dressed/static/js/2.3056a990.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f86d4c9f0455a68e391",
+    "url": "/possessed-and-dressed/static/js/main.1441b014.chunk.js"
+  },
+  {
+    "revision": "50351968c2e70fe125b5",
+    "url": "/possessed-and-dressed/static/js/runtime-main.9ec8c8c0.js"
+  },
+  {
+    "revision": "55a9f07289a97557ea35d7c202999feb",
+    "url": "/possessed-and-dressed/static/media/ghost_background.55a9f072.jpg"
+  },
+  {
+    "revision": "b7cbe552df6c3f48673d2c6ede3d6fcb",
+    "url": "/possessed-and-dressed/static/media/rainbow_background.b7cbe552.jpg"
+  }
+]);
