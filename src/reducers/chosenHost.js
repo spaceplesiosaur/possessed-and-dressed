64d4c543d54e3cd68f73e7ee6d43c3b82247345e
Allie McCarthy <mccarthy.allie@gmail.com>
2020-04-15 14:14:19 -0600

    Fix header so that it redirects to entryform

--- src/reducers/chosenHost.js
+++ src/reducers/chosenHost.js
@@ -2,6 +2,8 @@ export const chosenHostReducer = (state = {}, action) => {
   switch (action.type) {
     case 'CHOOSE_HOST':
       return action.chosenHost
+    case 'CLEAR_HOST':
+      return {}
     default:
       return state
   }
